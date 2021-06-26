2db06731325085605850623563cb59e6630881bc
Phu1237 <phu1237@gmail.com>
2021-06-26 07:56:27 +0700

    update manifest and wepack for Opera build

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,25 +10,39 @@ mix.options({
 });
 
 // Copy
-mix.copyDirectory('source/icons', 'dist/icons');
+// icons folder
+mix.copyDirectory('source/icons', 'dist/chromium/icons');
+mix.copyDirectory('source/icons', 'dist/opera/icons');
+// js files
+// chromium
 mix.copy([
     'source/manifest.json',
     'source/background.js'
-], 'dist');
+], 'dist/chromium');
+// opera
+mix.copy('source/background.js', 'dist/opera');
+mix.copy('source/manifest-opera.json', 'dist/opera/manifest.json');
+
+// src folder
 mix.copy([
     'source/src/popup.html',
     // 'source/src/options.html'
-], 'dist/src');
+], 'dist/chromium/src');
+mix.copy([
+    'source/src/popup.html',
+    // 'source/src/options.html'
+], 'dist/opera/src');
 
 // Compile
 // sass
-mix.sass('source/src/styles/app.scss', 'dist/src/popup.css')
+mix.sass('source/src/styles/app.scss', 'dist/chromium/src/popup.css')
 .options({
     postCss: [
         tailwindcss('tailwind.config.js'),
         require('autoprefixer')
     ]
 });
+mix.copy('dist/chromium/src/popup.css', 'dist/opera/src');
 // .purgeCss({
 //     content: [
 //         'source/src/**/*.js',
@@ -41,5 +55,7 @@ mix.sass('source/src/styles/app.scss', 'dist/src/popup.css')
 // ]);
 
 // js
-mix.copy('source/src/content.js', 'dist/src');
-mix.copy('source/src/popup.js', 'dist/src');
+mix.copy('source/src/content.js', 'dist/chromium/src');
+mix.copy('source/src/content.js', 'dist/opera/src');
+mix.copy('source/src/popup.js', 'dist/chromium/src');
+mix.copy('source/src/popup.js', 'dist/opera/src');
