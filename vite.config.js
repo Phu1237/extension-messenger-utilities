556fec5b4646daa68d701fedbf85dc3e0fe06823
Phu1237 <phu1237@gmail.com>
2022-02-04 15:05:34 +0700

    update vite config, add popup.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    open: '/index.html',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    watch: {},
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+        popup: path.resolve(__dirname, 'popup.html'),
+      },
+    },
+  },
 })
