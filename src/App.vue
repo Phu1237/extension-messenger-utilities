2d12ff89c955c35800ae1a0511476978dc48e5ae
Phu1237 <phu1237@gmail.com>
2022-02-04 15:17:36 +0700

    fix css not working

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,7 @@
 <template>
   <router-view />
 </template>
+
+<style lang="scss">
+@import "@/assets/app.scss";
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/app.scss'
 // Vue core
 import router from './router'
 // Plugins
