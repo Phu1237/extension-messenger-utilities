02a573f64df50a8e409bcea59a4a333a7b0ddb71
Phu1237 <phu1237@gmail.com>
2022-02-26 17:29:28 +0700

    vite build now just watch in dev mode

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,29 @@ import commonjs from '@rollup/plugin-commonjs'
 const path = require('path')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), commonjs()],
-  server: {
-    open: '/ui.html',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [vue(), commonjs()],
+    server: {
+      open: '/ui.html',
     },
-  },
-  build: {
-    watch: {},
-    rollupOptions: {
-      input: {
-        ui: path.resolve(__dirname, 'ui.html'),
-        content: path.resolve(__dirname, 'src/core/content.js'),
-        background: path.resolve(__dirname, 'src/core/background.js'),
-      },
-      output: {
-        entryFileNames: '[name].js',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    build: {
+      watch: mode === 'development' ? {} : false,
+      rollupOptions: {
+        input: {
+          ui: path.resolve(__dirname, 'ui.html'),
+          content: path.resolve(__dirname, 'src/core/content.js'),
+          background: path.resolve(__dirname, 'src/core/background.js'),
+        },
+        output: {
+          entryFileNames: '[name].js',
+        },
+      },
+    },
+  }
 })
